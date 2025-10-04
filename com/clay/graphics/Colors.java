71a673c28c208e0be56278896888c73654160e4b
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-10-04 21:19:01 +0800

    Update Colors.java

--- com/clay/graphics/Colors.java
+++ com/clay/graphics/Colors.java
@@ -1,4 +1,65 @@
 package com.clay.graphics;
 
 public class Colors {
+    private int value;
+
+    public static final int RED = 0xFFFF0000;
+    public static final int GREEN = 0xFF00FF00;
+    public static final int BLUE = 0xFF0000FF;
+    public static final int BLACK = 0xFF000000;
+    public static final int WHITE = 0xFFFFFFFF;
+    public static final int TRANSPARENT = 0x00000000;
+    public static final int YELLOW = 0xFFFFFF00;
+    public static final int CYAN = 0xFF00FFFF;
+    public static final int MAGENTA = 0xFFFF00FF;
+    public static final int GRAY = 0xFF808080;
+    public static final int SILVER = 0xFFC0C0C0;
+    public static final int MAROON = 0xFF800000;
+    public static final int OLIVE = 0xFF808000;
+    public static final int LIME = 0xFF00FF00;
+    public static final int TEAL = 0xFF008080;
+    public static final int NAVY = 0xFF000080;
+    public static final int PURPLE = 0xFF800080;
+    public static final int ORANGE = 0xFFFFA500;
+
+    public Colors(int value) {
+        this.value = value;
+    }
+
+    public static int argb(int alpha, int red, int green, int blue) {
+        return ((alpha & 0xFF) << 24) |
+                ((red & 0xFF) << 16) |
+                ((green & 0xFF) << 8) |
+                ((blue & 0xFF));
+    }
+
+    public static int rgb(int red, int green, int blue) {
+        return argb(255, red, green, blue);
+    }
+
+    public Color toAWTColor() {
+        return new Color(getRed(), getGreen(), getBlue(), getAlpha());
+
+
+    }
+
+    public int getRGB() {
+        return value;
+    }
+
+    public int getAlpha() {
+        return (getRGB() >> 24) & 0xFF;
+    }
+
+    public int getRed() {
+        return (getRGB() >> 16) & 0xFF;
+    }
+
+    public int getGreen() {
+        return (getRGB() >> 8) & 0xFF;
+    }
+
+    public int getBlue() {
+        return (getRGB()) & 0xFF;
+    }
 }
